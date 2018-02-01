305bf8739312d34bb9a5f57fa4ece0f080c2c381
mranjon007 <mithun.bishwash.cse@ulab.edu.bd>
2018-02-01 16:06:54 +0600

    insert_last function is added

---
+++ linked_list.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+
+
+struct Node{
+    int data;
+    Node* next;
+};
+
+Node* head = NULL;    // head should be global
+
+
+void insert_last(int data)
+{
+    //creating the new node
+    Node* inserting_node = new Node();
+    inserting_node->data = data;
+    inserting_node->next = NULL;
+
+    //if list is empty
+    if(head == NULL)
+    {
+        head = inserting_node;
+    }
+    else
+    {
+        // take head value to temp_node. as we don't want to loose head node value.
+        Node* temp_node = head;
+        while(temp_node->next != NULL)      // this loop run to the last position of the list
+        {
+            temp_node = temp_node->next;    // change the temp_node to the next node of the linked list
+        }
+        temp_node->next = inserting_node;   // change the link value of the last node to address of new inserting_node
+    }
+}
+
+
+void print_linked_list()
+{
+    Node* temp_node = head;
+    while(temp_node != NULL)
+    {
+        cout<< temp_node->data;
+        cout << "  ";
+        temp_node = temp_node->next;
+    }
+    cout << endl;
+}
+
+
+int main()
+{
+    return 0;
+}
